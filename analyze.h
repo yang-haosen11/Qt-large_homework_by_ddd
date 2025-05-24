3e1d6039e28a1cf023fb6e5b717683a0db4e33b7
yang-haosen11 <yhs2400017813@outlook.com>
2025-05-24 15:17:54 +0800

    Add files via upload
    analyze upgrade

---
+++ analyze.h
@@ -0,0 +1,68 @@
+﻿#ifndef ANALYZE_H
+#define ANALYZE_H
+
+#include "function.h"
+#include <QEvent>
+#include <vector>
+#include <string>
+#include <QSqlDatabase>
+#include <iostream>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include<QFile>
+#include <QChart>
+#include <QPieSeries>
+#include<QAbstractSeries>
+#include <QPieSlice>
+#include <QChartView>
+#include <QTextStream>
+#include <windows.h>
+#pragma push_macro("slots")
+#undef slots //用于由于slots在Qt和Python都是关键字，互相冲突
+#include <Python.h>
+#pragma pop_macro("slots")
+
+
+#include<QtCharts>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class
+    Analyze;
+}
+QT_END_NAMESPACE
+
+
+
+class Analyze : public Function
+{
+    Q_OBJECT
+private:
+    QFile file;
+    std::string question;
+    Ui::Analyze *ui;
+    QList<QSqlRecord> result_month;
+    QAbstractSeries *currentSeries;
+    QChartView *currentChartView;
+    QScrollArea *scrollArea;
+    QLabel *label;
+    const char* AIresult;
+public:
+    Analyze(QWidget *parent = nullptr);//构造函数，实现分析类
+    void FileToDB();//存储文件读取
+    void MakeQuestion();//将文件信息转化可以投给大模型的问题
+    int AIAnalyze();//调用API分析账单并提供建议
+    void MakeWeekTreeChart();
+    void MakeWeekPieChart();
+    void MakeMonthTreeChart();
+    void MakeMonthPieChart();
+    void DeletePreChart();
+    //制表：每周、每月树状、饼状图，共计四张
+    bool initDatabase();//数据库创建
+    void GetLastMonthData();
+public slots:
+    void MakeCharts(int flag);
+    void AItext();
+};
+
+#endif // ANALYZE_H
